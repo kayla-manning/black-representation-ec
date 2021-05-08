9b7e1d6f52dc329d4a621d139d6048bfe26e1a74
Kayla Manning <kaylamanning@college.harvard.edu>
2021-05-08 19:22:36 -0400

    fixed code to have percentage rather than proportion & addd report doc

--- data/ipums/ipums.R
+++ data/ipums/ipums.R
@@ -1,10 +1,10 @@
 # NOTE: To load data, you must download both the extract's data and the DDI
 # and also set the working directory to the folder with these files (or change the path below).
 
-if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
-
-ddi <- read_ipums_ddi("data/ipums/usa_00008.xml")
-data <- read_ipums_micro(ddi)
+# if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
+# 
+# ddi <- read_ipums_ddi("data/ipums/usa_00008.xml")
+# data <- read_ipums_micro(ddi)
 
 # getting ready to clean the data
 
@@ -18,9 +18,12 @@ data <- read_ipums_micro(ddi)
   map <- purrr::map
 }
 
-# getting everything grouped by state
+# getting everything grouped by state... also I realized that 1980, 1990, 2000
+# numbers do not make sense (it appears that they're a factor of 10 greater than
+# they should be), so I am getting them all to the same baseline & then
+# rescaling to the actual population count
 
-race_counts <- data %>% 
+old_race_counts <- data %>% 
   clean_names() %>%
   select(-c(age)) %>% 
   group_by(year, statefip, race) %>% 
@@ -28,15 +31,45 @@ race_counts <- data %>%
   mutate(statefip = as_label(statefip),
          race = as_label(race)) %>% 
   rename(state = statefip) %>% 
-  ungroup()
+  ungroup() %>% 
+  mutate(n = case_when(year %in% c(1980, 1990, 2000) ~ n / 10 * 100,
+                       TRUE ~ n * 100))
+
+{
+  # inferring data points for years not in the IPUMS data... race coding is
+  # different starting at the turn of the century, so I have to store vectors for
+  # each of these
+  
+  races <- unique(race_counts$race[race_counts$year == 1970])
+  missing_years <- c(1971:1979, 1981:1989, 1991:1999)
+  
+  # creating table with extra years... 2004 numbers look very wrong when
+  # compared to other years, so I am testing out what happens if I remove them
+  # do linear interpolation on those too
+  
+  extra_race_counts <- tibble(year = rep(missing_years, each = length(state.name) * length(races)),
+                              state = rep(state.name, each = 7, times = length(missing_years)),
+                              race = rep(races, times = length(state.name) * length(missing_years)),
+                              n = NA) %>% 
+    bind_rows(old_race_counts) %>% 
+    mutate(n = ifelse(year == 2004, NA, n))
+  
+  # using linear interpolation to approximate the counts for each race within each state/year
+  
+  race_counts <- extra_race_counts %>% 
+    arrange(state, race, year) %>% 
+    mutate(n = na.approx(n))
+}
 
 # dropping NA state values (which is just DC) & creating my black proportion
 # columns for the main analysis
 
-black_ipums_1 <- race_counts %>% 
+black_ipums <- race_counts %>% 
+  filter(!str_detect(race, "major races")) %>% 
   mutate(race = ifelse(str_detect(race, "Black"), "black", "nonblack")) %>% 
   group_by(year, state, race) %>% 
-  summarise(n = sum(n)) %>% 
+  summarise(n = sum(n),
+            .groups = "drop") %>% 
   pivot_wider(names_from = race, values_from = n) %>% 
   mutate(total_vap = black + nonblack,
          black_prop_vap = black / (black + nonblack),
@@ -44,62 +77,62 @@ black_ipums_1 <- race_counts %>%
   select(year, state, black_prop_vap, black, total_vap) %>% 
   drop_na(state) %>% 
   ungroup()
+
+# replacing the total VAP numbers for 2000 with what I read in from the pdf
+
+
+{
+  # 2000 data is wrong for Georgia, so I am not sure that i can trust it for other
+  # states. because of that, I am reading the numbers in from this PDF
   
-# getting 2004 data since the ACS numbers seem very wrong... realized data is in
-# thousands from the Census website (it's likely that's how it is with the ipums
-# data too, but I will stick with the census data since I know that for a fact)
+  dat <- tabulizer::extract_tables("https://www.eac.gov/sites/default/files/eac_assets/1/6/2000%20Voter%20Registration%20and%20Turnout%20by%20State.pdf",
+                                   area = list(c(233.820958, 7.900537, 768.296910, 597.055877),
+                                               c(22.147313, 7.900537, 369.644880, 582.944372 )))
+  vap2000 <- do.call(rbind, dat) %>% 
+    as_tibble() %>% 
+    select(V1, V2) %>% 
+    mutate(V2 = as.numeric(str_remove_all(V2, ",")),
+           V1 = state.abb[match(V1, state.name)]) %>% 
+    rename(state = V1, total_vap = V2) %>% 
+    drop_na(state)
+}
 
-dat2004 <- read_excel("data/ipums/vap2004.xls", skip = 4) %>% 
-  clean_names() %>% 
-  select(state_sex_race_and_hispanic_origin,
-         population_18_and_over) %>% 
-  drop_na(state_sex_race_and_hispanic_origin) %>% 
-  mutate(state = ifelse(!str_detect(state_sex_race_and_hispanic_origin, "[a-z]"),
-                        state_sex_race_and_hispanic_origin,
-                        NA),
-         state = na.locf(state)) %>% 
-  rename(cat = state_sex_race_and_hispanic_origin,
-         vap = population_18_and_over) %>% 
-  mutate(cat = str_remove_all(cat, fixed(".")),
-         year = 2004) %>% 
-  filter(cat != state,
-         !cat %in% c("Male", "Female")) %>% 
-  mutate(state = state.abb[match(str_to_title(state), state.name)],
-         vap = as.numeric(vap) * 10) %>% 
-  drop_na(state) %>% 
-  ungroup()
+# using this data for 1980-2014
 
-# formatting 2004 data to match the black_ipums table
+dat <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1or-N33CpOZYQ1UfZo0h8yGPSyz0Db-xjmZOXg3VJi-Q/edit#gid=1670431880") %>% 
+  clean_names()
 
-black_2004 <- dat2004 %>% 
-  mutate(cat = case_when(str_detect(cat, "Black") ~ "black", 
-                         cat == "Total" ~ "total_vap",
-                         TRUE ~ "other")) %>% 
-  filter(cat != "other") %>% 
-  group_by(cat, state, year) %>% 
-  summarise(vap = sum(vap),
-            .groups = "drop") %>% 
-  pivot_wider(names_from = cat, values_from = vap) %>% 
-  mutate(black_prop_vap = black / total_vap) %>% 
-  ungroup()
+# unlisting the columns that I need & putting them in a tibble
 
-# replacing ipums 2004 data with this.... also I realized that 1980, 1990, 2000
-# numbers do not make sense (it appears that they're a factor of 10 greater than
-# they should be), so I am getting them all to the same baseline & then
-# rescaling to the actual population count
+vaps <- tibble(year = unlist(dat$x1),
+       state = unlist(dat$state),
+       total_vap = unlist(dat$x11)) %>% 
+  mutate(year = as.numeric(year),
+         total_vap = as.numeric(total_vap),
+         state = state.abb[match(state, state.name)]) %>% 
+  drop_na()
 
-black_ipums <- black_ipums_1 %>% 
-  filter(year != 2004) %>% 
-  bind_rows(black_2004) %>% 
-  mutate(black = case_when(year %in% c(1980, 1990, 2000) ~ black / 10 * 100,
-                           TRUE ~ black * 100),
-         total_vap = case_when(year %in% c(1980, 1990, 2000) ~ total_vap / 10 * 100,
-                           TRUE ~ total_vap * 100)) %>% 
-  ungroup()
+black_ipums <- black_ipums %>% 
+  filter(year %in% seq(1976, 2016, by = 4))
+
+# replacing all vap data from 1980 to 2014 with this data
+
+for (i in 1:nrow(black_ipums)) {
+  
+  state <- black_ipums[i,]$state
+  year <- black_ipums[i,]$year
+  
+  # replacing total_vap and updating black vap accordingly (proportion still
+  # appears to be correct)
+  
+  if (year %in% 1980:2014) {
+    black_ipums[i,]$total_vap <- vaps$total_vap[vaps$state == state & vaps$year == year]
+    black_ipums[i,]$black <- black_ipums[i,]$black_prop_vap * black_ipums[i,]$total_vap
+  }
+}
 
 # saving the data I use as CSV files so that I don't have to rerun this script
 # (it takes a long time)
 
 write_csv(black_ipums, "data/ipums/saved_data/black_ipums.csv")
-write_csv(race_counts, "data/ipums/saved_data/race_counts.csv")
 
